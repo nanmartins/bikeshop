ec224d1c6975936880a555446b4ab22269405654
Renan Martins <renanmartins989@gmail.com>
2022-06-29 14:22:46 +0100

    basic seed with initial models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# PRODUCTS
+puts 'Deleting all products'
+Product.destroy_all
+product1 = Product.create({name: 'Classic bike', price: 400})
+product2 = Product.create({name: 'Mountain bike', price: 300})
+product3 = Product.create({name: 'Downhill bike', price: 700})
+
+puts "Number of products: #{Product.all.count}"
+puts "Product names: #{Product.all.pluck('name')}"
+puts "Product1: #{product1.name} price: #{product1.price.round(2)}"
+puts "Product2: #{product2.name} price: #{product2.price.round(2)}"
+puts "Product3: #{product3.name} price: #{product3.price.round(2)}"
+
+
+# CART
+Cart.destroy_all
+puts "\nTotal cart count: #{Cart.all.count}"
